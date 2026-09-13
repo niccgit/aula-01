71a778b262067170aae0314cc795a85f6fee5981
Nicolly J. Lima <nicollylima747@outlook.com.br>
2026-09-13 18:10:40 -0300

    atualização na estrutura do código
    código 02.py - aula 01 de lopal

--- 02.py
+++ 02.py
@@ -1,14 +1,16 @@
-# Crie um algoritmo que leia 3 valores (lados de um triângulo)
-# Determine se formam um triângulo, e se formar verifique
-# se é um equilátero, isósceles ou escaleno.
+# Crie um algoritmo em Python que leia 3 valores (lados de um triângulo).
+#
+# Primeiro determine se esses valores são suficientes para formar um triângulo.
+# Caso sejam suficientes, verifique se é um triângulo Equilátero, Isósceles ou Escaleno.
 
-a = int(input("Digite o lado A: "))
-b = int(input("Digite o lado B: ")) 
-c = int(input("Digite o lado C: ")) 
 
-if (a + b > c) and (a + c > b) and (b + c > a):
+a = float(input("Digite o lado A: "))
+b = float(input("Digite o lado B: ")) 
+c = float(input("Digite o lado C: ")) 
+
+if (a + b > c) and (a + c > b) and (b + c > a): 
     print("Forma um triângulo: ")
-    if a == b and a == c and b == c:
+    if a == b and a == c and b == c: 
         print("Equilátero")
     elif a == b or a == c or b == c:
         print("Isósceles")
@@ -16,3 +18,12 @@ if (a + b > c) and (a + c > b) and (b + c > a):
         print("Escaleno")
 else:
      print("Não forma um triângulo!")
+
+
+# (a + b > c) and (a + c > b) and (b + c > a (é a condição de existência de qualquer triângulo: cada lado precisa ser menor que a soma dos outros dois lados)
+#
+# a == b and a == c and b == c (é a condição de existência do triângulo equilátero: todos os lados devem ser iguais)
+#
+# a == b or a == c or b == c é a condição de existência do triângulo isósceles: pelo menos dois lados devem ser iguais)
+#
+# a != b and a != c and b != c é a condição de existência do triângulo escaleno: todos os lados devem ser diferentes)
